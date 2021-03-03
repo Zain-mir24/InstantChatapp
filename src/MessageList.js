d7b47b8f08288c25d0154da4569713d8b6ee09f0
Zainmir <zainnaeemk10@gmail.com>
2021-03-03 23:00:13 +0000

    working

--- src/MessageList.js
+++ src/MessageList.js
@@ -1,17 +1,37 @@
-import React from 'react'
-const stylingchatList={
-    height:"800px",
-    width:"100%",
-    backgroundColor:"#03a9f4",
-
+import React from "react";
+const stylingchatList = {
+  height: "800px",
+  width: "100%",
+  backgroundColor: "#03a9f4",
+};
+const stylingSidebar = {
+  backgroundColor: "#3d5afe",
+  padding: "25px",
+  width: "10%",
+  height: "90%",
+  float: "left",
+};
+const messagedialogue={
     
 }
 function MessageList() {
-    return (
-        <div style={stylingchatList}>
-        
-        </div>
-    )
+  return (
+    <div style={stylingchatList}>
+      <div className="SideBar" style={stylingSidebar}>
+        <h3>Our Room</h3>
+        <h4>our users</h4>
+        <ul>
+          <li>users</li>
+        </ul>
+      </div>
+      <div className="message">
+        <p>
+          <span className="message__name">username</span>
+        </p>
+        <p>message</p>
+      </div>
+    </div>
+  );
 }
 
-export default MessageList
+export default MessageList;
