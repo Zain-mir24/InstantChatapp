378d9b208cb171e7e3fc75538bb65e1216f11514
Zainmir <zainnaeemk10@gmail.com>
2021-03-05 19:28:06 +0000

    adding back end now

--- src/App.js
+++ src/App.js
@@ -8,7 +8,18 @@ const path = require('path')
 const express= require('express')
 const socketio = require('socket.io')
 const http = require('http')
+const app = express()
+const server =http.createServer(app)
+const io=socketio(server)
+const port=process.env.PORT || 3000
 
+const publicDirectoryPath = path.join(__dirname,'../public')
+app.use(express.static(publicDirectoryPath)) 
+
+
+io.on('connection',(socket)=>{
+
+})
 function App() {
   const [message, Setmessage] = React.useState("");
   return (
@@ -28,5 +39,8 @@ function App() {
   );
 }
 // app.js
+server.listen(port,()=>{
+  console.log(`Server is up on port ${port}!`)
+})
 
 export default App;
