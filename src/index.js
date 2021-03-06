70fd27133e15afb17119ec2801bf1e9a68a7a40d
Zainmir <zainnaeemk10@gmail.com>
2021-03-06 22:03:18 +0000

    frontend fixing

--- src/index.js
+++ src/index.js
@@ -2,26 +2,7 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
-// const express= require('express')
-// const socketio = require('socket.io')
 
-// const app = express()
-
-// const io=socketio(server)
-// const { Socket } = require('dgram')
-// const port=process.env.PORT ||3001
-
-// const publicDirectoryPath = path.join(__dirname,'../public')
-// app.use(express.static(publicDirectoryPath)) 
-
-
-// io.on('connection',(socket)=>{
-
-// })
-
-// app.listen(port,()=>{
-//   console.log(`Server is up on port ${port}!`)
-// })
 
 ReactDOM.render(
   <React.StrictMode>
